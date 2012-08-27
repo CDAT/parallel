4bd811e9ce411f5666d4269673019616a6953921
Charles Doutriaux <doutriaux1@llnl.gov>
2012-08-27 09:07:27 -0700

    allow input of cpus to run for multiruns

--- DistArray/multiruns.py
+++ DistArray/multiruns.py
@@ -2,7 +2,13 @@ import sys,os
 
 test = sys.argv[1]
 
-for n in [1,2,3,4,6,8]:
+if len(sys.argv)>2:
+    cpus=[]
+    for a in sys.argv[2:]:
+        cpus.append(int(a))
+else:
+    cpus = [1,2,3,4,6,8] 
+for n in cpus:
     for method in ["stagger","blocks"]:
         cmd = "mpiexec -n %i python %s %s" % (n,test,method)
         for i in range(5):
